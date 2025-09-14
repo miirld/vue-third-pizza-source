812a5afb17bd8d5f8ca2cf3d9b3c5bcd8e6656f5
Mikhail Rudakov (Work) <miirld0302@gmail.com>
2025-09-14 20:27:16 +0300

    Task 1.34 v2

--- course/section0/frontend/src/layouts/AppHeader.vue
+++ course/section0/frontend/src/layouts/AppHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="header">
     <div class="header__logo">
-      <a href="index.html" class="logo">
+      <a href="/" class="logo">
         <img
-          src="img/logo.svg"
+          src="@/assets/img/logo.svg"
           alt="V!U!E! Pizza logo"
           width="90"
           height="40"
@@ -11,10 +11,10 @@
       </a>
     </div>
     <div class="header__cart">
-      <a href="cart.html">0 ₽</a>
+      <a href="/">0 ₽</a>
     </div>
     <div class="header__user">
-      <a href="#" class="header__login"><span>Войти</span></a>
+      <a href="/" class="header__login"><span>Войти</span></a>
     </div>
   </header>
 </template>
